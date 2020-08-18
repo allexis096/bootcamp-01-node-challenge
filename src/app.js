06f5e18050f8717ac9be137981cb6c5304220557
allexis096 <allexissoares@gmail.com>
2020-08-18 20:57:52 -0300

    :heavy_check_mark: test: update likes repository done

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ app.post("/repositories", (request, response) => {
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  const { title, url, techs, likes } = request.body;
+  const { title, url, techs } = request.body;
 
   const repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
